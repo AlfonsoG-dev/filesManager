9848818efa7707761a3c70344961805e02770d41
tuNovio <alfgomachi@gmail.com>
2023-11-06 13:31:16 -0500

    refactor: adicionar a las opciones el mover files

--- src/App.java
+++ src/App.java
@@ -4,13 +4,19 @@ class App {
             outter:for(int i=0; i<args.length; ++i) {
                 Operaciones op = new Operaciones("./");
                 switch(args[i]) {
-                    case "-lc": 
+                    case "-ls": 
                         op.ChangeDirectory(args[i+1]);
                         op.ListFiles();
                         break;
                     case "-cp":
                         if((i+2) < args.length && args[i+2].toLowerCase().equals("to") && (i+3) < args.length) {
                             op.CopyFilesfromSourceToTarget(args[i+1], args[i+3]);
+                        }
+                        break;
+                    case "-mv":
+                        if((i+1) < args.length && args[i+2].equals("to") && (i+3) < args.length) {
+                            System.out.println("p ==");
+                            op.MoveFilesFromSourceToTarget(args[i+1], args[i+3]);
                         }
                         break;
                     case "-md":

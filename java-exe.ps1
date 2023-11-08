9b0e967281b4969c528eebb290208ab28f0cc910
tuNovio <alfgomachi@gmail.com>
2023-11-08 00:21:48 -0500

    refactor: dar nombre apropiado a -jar file

--- java-exe.ps1
+++ java-exe.ps1
@@ -1,7 +1,7 @@
 $clases = " ./src/*.java ./src/Utils/*.java ./src/Mundo/*.java"
 $compile = "javac -d ./bin/" + "$clases"
 $javaCommand = "java -jar test.jar"
-$createJarFile = "jar -cfm test.jar Manifesto.txt -C ./bin/ ."
+$createJarFile = "jar -cfm filesManager.jar Manifesto.txt -C ./bin/ ."
 $runCommand = "$compile" + " && " + "$createJarFile" + " && " + "$javaCommand"
 
 Invoke-Expression $runCommand

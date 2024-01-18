d3770bcaf22276d33ffe02032dd063f1bfb1f2da
tuNovio <alfgomachi@gmail.com>
2024-01-18 12:59:25 -0500

    refactor: add a thread to main class for execution

--- src/filesManager.java
+++ src/filesManager.java
@@ -1,7 +1,14 @@
 import Mundo.Operaciones;
 class filesManager {
     public static void main(String[] args) {
-        Operaciones misOperaciones = new Operaciones();
-        misOperaciones.OrganizeOptions(args);
+        Thread miThread = new Thread("aplication");
+        try {
+            miThread.start();
+            Operaciones misOperaciones = new Operaciones();
+            misOperaciones.OrganizeOptions(args);
+        } catch(Exception e) {
+            miThread.interrupt();
+            e.printStackTrace();
+        }
     }
 }

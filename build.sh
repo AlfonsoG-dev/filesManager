bba1b6b5bc0093a412be9ae672f50a52ae7d4fde
AlfonsoG-dev <alfgomachi@gmail.com>
2024-08-06 17:20:12 -0500

    feat: add sh build script for linuz

---
+++ build.sh
@@ -0,0 +1,5 @@
+srcClases = "./src/*.java ./src/Utils/*.java ./src/Mundo/*.java ./src/Utils/Verifications/*.java "
+libFiles = ""
+javac -Werror -Xlint:all -d ./bin/ $srcClases
+jar -cfm filesManager.jar Manifesto.txt -C ./bin/ .
+java -jar filesManager.jar
--- java-exe.ps1
+++ java-exe.ps1
@@ -1,5 +1,7 @@
-$compile = "javac -Xlint:all -d .\bin\ .\src\*.java -sourcepath .\src\"
+$srcClases = ".\src\*.java .\src\Utils\*.java .\src\Mundo\*.java .\src\Utils\Verifications\*.java "
+$libFiles = ""
+$compile = "javac -Werror -Xlint:all -d .\bin\ $srcClases"
 $createJar = "jar -cfm filesManager.jar Manifesto.txt -C .\bin\ ."
 $javaCommand = "java -jar filesManager.jar"
 $runCommand = "$compile" + " && " + "$createJar" + " && " +"$javaCommand"
-Invoke-Expression $runCommand
+Invoke-Expression $runCommand 

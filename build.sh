2c8758a268821ef7a2b27155bb57ea7309736291
AlfonsoG-dev <alfgomachi@gmail.com>
2024-08-18 18:09:03 -0500

    refactor: file separator & sh build file syntax

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
-srcClases = "./src/*.java ./src/Utils/*.java ./src/Mundo/*.java ./src/Utils/Verifications/*.java "
-libFiles = ""
+srcClases="./src/*.java ./src/Utils/*.java ./src/Mundo/*.java ./src/Utils/Verifications/*.java "
+libFiles=""
 javac -Werror -Xlint:all -d ./bin/ $srcClases
 jar -cfm filesManager.jar Manifesto.txt -C ./bin/ .
 java -jar filesManager.jar
